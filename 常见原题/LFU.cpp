35453ed206f74396b53a5907fbff1bf2720a17c6
bodhicheng <nwct@qq.com>
2020-07-19 16:30:54 +0800

    20200719 LRU&LFU

--- 常见原题/LFU.cpp
+++ 常见原题/LFU.cpp
@@ -1,73 +1,67 @@
-/*
-leetcode 460
-*/
 class LFUCache {
-    HashMap<Integer, Node> cache;
-    HashMap<Integer, LinkedHashSet<Node>> map;
-    int c,min;
-    public LFUCache(int capacity) {
-        cache = new HashMap<>(capacity);
-        map = new HashMap<>();
-        c = capacity;
-    }
-
-    public int get(int key) {
-        Node node = cache.get(key);
-        if(node == null) {
-            return -1;
-        }
-        freqInc(node);
-        return node.value;
-    }
-
-    public void put(int key, int value) {
-        if(c == 0) return ;
-        Node node = cache.get(key);
-        if(node != null) {
-            node.value = value;
-            freqInc(node);
-        } else {
-            node = new Node(key,value);
-            if(cache.size() == c) removeNode();
-            addNode(node);
-        }
-    }
-    void addNode(Node node) {
-        cache.put(node.key,node);
-        LinkedHashSet<Node> set = map.get(node.freq);
-        if(set == null) {
-            set = new LinkedHashSet<>();
-            map.put(node.freq,set);
-        }
-        set.add(node);
-        min = 1;
-    }
-    void removeNode () {
-        LinkedHashSet<Node> set = map.get(min);
-        Node dead = set.iterator().next();
-        cache.remove(dead.key);
-        set.remove(dead);
-
-    }
-    void freqInc(Node node) {
-        int freq = node.freq;
-        LinkedHashSet<Node> set = map.get(freq);
-        set.remove(node);
-        if(freq == min && set.isEmpty()) {
-            min = freq+1;
-        }
-        node.freq++;
-        LinkedHashSet<Node> newSet = map.computeIfAbsent(freq + 1, k -> new LinkedHashSet<>());
-        newSet.add(node);
-    }
-    class Node {
+public:
+    struct Node {
         int key;
         int value;
         int freq = 1;
+        list<Node>::iterator iter;
         Node() {}
-        Node(int k,int v) {
-            this.key = k;
-            this.value = v;
+        Node(int k,int v):key(k),value(v) {}
+        bool operator ==(const Node &x) {
+            return key == x.key;
+        }
+    };
+    int Capacity;
+    int mn;
+    unordered_map<int,Node> cache;
+    unordered_map<int,list<Node> > freqMap;
+    LFUCache(int capacity) {
+        Capacity = capacity;
+    }
+
+    int get(int key) {
+        if(cache.count(key)) {
+            Node &node = cache[key];
+            freqInc(node);
+            return node.value;
+        } else return -1;
+    }
+
+    void put(int key, int value) {
+        if(Capacity == 0) return ;
+        if(cache.count(key)) {
+            Node &node = cache[key];
+            node.value = value;
+            freqInc(node);
+        } else {
+            if(cache.size() == Capacity) removeNode();
+            Node node = Node(key,value);
+            addNode(node);
         }
     }
-}
+
+    void addNode(Node &node) {
+        list<Node> &v = freqMap[1];
+        v.push_front(node);
+        node.iter = v.begin();
+        cache[node.key] = node;
+        mn = 1;
+    }
+
+    void removeNode() {
+        list<Node> &v = freqMap[mn];
+        Node &node = v.back();
+        cache.erase(node.key);
+        v.pop_back();
+    }
+
+    void freqInc(Node &node) {
+        list<Node> &v = freqMap[node.freq];
+        v.erase(node.iter);
+
+        if(node.freq == mn && v.size() == 0) mn++;
+        node.freq++;
+        freqMap[node.freq].push_front(node);
+        node.iter = freqMap[node.freq].begin();
+    }
+};
--- 常见原题/LRU.cpp
+++ 常见原题/LRU.cpp
@@ -1,38 +1,54 @@
-/*
-leetcode 146
-*/
 class LRUCache {
-    LinkedHashMap<Integer,Integer> map;
-    int Capacity = -1;
-    public LRUCache(int capacity) {
-        map = new LinkedHashMap<>(capacity);
+public:
+    struct Node {
+        int key;
+        int value;
+        list<Node>::iterator iter;
+        Node() {}
+        Node(int k,int v):key(k),value(v) {}
+    };
+    unordered_map<int,Node> cache;
+    list<Node> s;
+    int Capacity;
+    LRUCache(int capacity) {
         Capacity = capacity;
     }
-    
-    public int get(int key) {
-        if(map.containsKey(key)) {
-            int v = map.get(key);
-            update(key,v);
-            return map.get(key);
-            
+
+    int get(int key) {
+        if(cache.count(key)) {
+            Node &node = cache[key];
+            update(node);
+            return node.value;
         } else return -1;
     }
-    
-    public void put(int key, int value) {
-        if(Capacity <= 0) return ;
-        if(map.containsKey(key)) {
-            update(key,value);
-            return ;
+
+    void put(int key, int value) {
+        if(cache.count(key)) {
+            Node &node = cache[key];
+            node.value = value;
+            update(node);
+        } else {
+            if(Capacity == 0) return ;
+            if(Capacity == cache.size()) removeNode();
+            Node node = Node(key,value);
+            addNode(node);
         }
-        if(map.size() == Capacity) remove();
-        map.put(key,value);
-        
     }
-    void remove() {
-        map.remove(map.entrySet().iterator().next().getKey());
+
+    void addNode(Node &node) {
+        s.push_front(node);
+        node.iter = s.begin();
+        cache[node.key] = node;
     }
-    void update(int key,int v) {
-        map.remove(key);
-        map.put(key,v);
+
+    void removeNode() {
+        Node &node = s.back();
+        cache.erase(node.key);
+        s.pop_back();
     }
-}
+    void update(Node &node) {
+        s.erase(node.iter);
+        s.push_front(node);
+        node.iter = s.begin();
+    }
+};
